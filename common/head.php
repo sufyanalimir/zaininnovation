d0d97538698a3e3e90d5cfe3154aefba3245f8ca
Mir Sufyan Ali <sufyanalimir313@gmail.com>
2024-03-28 18:04:20 +0530

    created dev environment

---
+++ common/head.php
@@ -0,0 +1,11 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zain Innovation Pvt Ltd</title>
+    <link rel="icon" href="assets/images/cropped-Letter-head-01-02-192x192.png" sizes="192x192">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="assets/lib/owl-carousel/owl-carousel.min.css">
+    <link rel="stylesheet" href="assets/lib/animate/animate.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+    include("common/head.php");
+    ?>
+<body>
+    
+</body>
+</html>
